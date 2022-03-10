f0966d91ae3bee12321e7359b25ae6deabc86c30
kleberkaerk <apenaskleber123@gmail.com>
2022-03-10 17:48:33 -0300

    Adicionando recursos

---
+++ src/main/java/br/com/pizzariazeff/webserver/WebServerPizzariaZeffApplication.java
@@ -0,0 +1,13 @@
+package br.com.pizzariazeff.webserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServerPizzariaZeffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServerPizzariaZeffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pizzariazeff/webserver/WebServerPizzariaZeffApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pizzariazeff.webserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServerPizzariaZeffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
